a9531375d32a743f127325707e1fba6b5bce3f3d
callum.zhong <callum.zhong@gmail.com>
2025-06-20 04:05:25 +0800

    fix(config): fix comment formatting in options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,6 @@
 vim.wo.number = true -- Make line numbers default (default: false)
 vim.o.relativenumber = true -- Set relative numbered lines (default: false)
--- vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim. (default: '')
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim. (default: '')
 -- Configure OSC 52 clipboard for Docker/remote environments
 vim.g.clipboard = {
   name = 'OSC 52',
