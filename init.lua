31072704ba7a76ba438841ce8e7f32f27b4b93c2
callum.zhong <callum.zhong@gmail.com>
2025-07-23 20:54:29 +0800

    feat: add oil.nvim and fzf-lua plugins
    - Add oil.nvim for file exploration
    - Add fzf-lua for fuzzy finding

--- init.lua
+++ init.lua
@@ -14,16 +14,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-  require 'plugins.neotree',
   require 'plugins.colortheme',
   require 'plugins.bufferline',
   require 'plugins.lualine',
   require 'plugins.treesitter',
-  require 'plugins.telescope',
   require 'plugins.gitsigns',
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
   require 'plugins.misc',
   require 'plugins.comment',
   require 'plugins.lazygit',
+  require 'plugins.oil',
+  require 'plugins.fzf-lua'
 }
